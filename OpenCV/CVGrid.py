f4fa201171f5e62f2d27fcb010038ca0e86ff5cb
Casper Hartmann Blichfedt <Casper.h.blichfeldt@live.dk>
2024-06-18 12:45:07 +0200

    Modified CVGrid: Should find nearest non 0 grid for the balls before they are added to the grid in for the bfs from robot to balls so that they will not be blocked in, now there should always be a path to the balls in a*

--- OpenCV/CVGrid.py
+++ OpenCV/CVGrid.py
@@ -1,15 +1,17 @@
-from bfsAlgo import bfs
+from bfsAlgo import bfs, find_nearest_non_zero
 from aStarAlgo import a_star_search
 from collections import deque
+
 
 class CVGrid:
 
     def __init__(self, grid_size, frame_size) -> None:
         self.grid_size = grid_size
         self.frame_size = frame_size
-        self.cell_size = (int(frame_size[0]/grid_size[0]), int(frame_size[1]/grid_size[1]))
-        self.cells = [[CVCell((y,x),(x*self.cell_size[0], y*self.cell_size[1]), "unblocked") for x in range(grid_size[0])] for y in range(grid_size[1])]
-
+        self.cell_size = (
+            int(frame_size[0]/grid_size[0]), int(frame_size[1]/grid_size[1]))
+        self.cells = [[CVCell((y, x), (x*self.cell_size[0], y*self.cell_size[1]), "unblocked")
+                       for x in range(grid_size[0])] for y in range(grid_size[1])]
 
     def block_out_of_bounds(self, boundary):
         def is_point_in_polygon(x, y, poly):
@@ -30,7 +32,7 @@ class CVGrid:
                 px, py = sx, sy
 
             return inside
-        
+
         # Get the polygon in pixel coordinates
         poly = [
             boundary["top_left"],
@@ -45,7 +47,6 @@ class CVGrid:
                 if not is_point_in_polygon(cell_center[1], cell_center[0], poly):
                     cell.status = "blocked"
 
-    
     def block_at_pixel_position(self, position):
         x, y = position
         for column in self.cells:
@@ -55,7 +56,7 @@ class CVGrid:
                 if cell_x <= x < cell_x + cell_width and cell_y <= y < cell_y + cell_height:
                     cell.status = "blocked"
                     return
-                
+
     def get_position_from_pixel(self, position):
         x, y = position
         for column in self.cells:
@@ -64,12 +65,10 @@ class CVGrid:
                 cell_width, cell_height = self.cell_size
                 if cell_x <= x < cell_x + cell_width and cell_y <= y < cell_y + cell_height:
                     return cell.position
-                
+
     def get_pixel_from_position(self, position):
         cell_width, cell_height = self.cell_size
         return [int(int(cell_width/2) + cell_width * position[0]), int(int(cell_height/2) + cell_height * position[1])]
-
-
 
     def clearBlocks(self):
         for x in self.cells:
@@ -83,25 +82,29 @@ class CVGrid:
                 if cell.status == "blocked":
                     for dy in range(-neighbors, neighbors + 1):
                         for dx in range(-neighbors, neighbors + 1):
-                            ny, nx = cell.position[0] + dy, cell.position[1] + dx
+                            ny, nx = cell.position[0] + \
+                                dy, cell.position[1] + dx
                             if 0 <= ny < self.grid_size[1] and 0 <= nx < self.grid_size[0]:
                                 to_block.add((ny, nx))
         for ny, nx in to_block:
             self.cells[ny][nx].status = "blocked"
 
+    # Give destination and source arrays in grid coordinates.
 
-
-    ## Give destination and source arrays in grid coordinates.
     def navigate(self, destination, source):
-        
+
         # Define the grid (1 for unblocked, 0 for blocked)
-        grid = [[1 for _ in range(self.grid_size[0])] for _ in range(self.grid_size[1])]
-        
+        grid = [[1 for _ in range(self.grid_size[0])]
+                for _ in range(self.grid_size[1])]
+
         # coordinater for forhindringer bliver sat til 0 i grid
         for y in range(self.grid_size[0]):
             for x in range(self.grid_size[1]):
                 if self.cells[x][y].status == "blocked":
                     grid[x][y] = 0
+
+        for i in range(len(destination)):
+            destination[i] = find_nearest_non_zero(grid, destination[1])
 
         value = 3
         for n in destination:
@@ -112,7 +115,7 @@ class CVGrid:
         result = bfs(grid, start)
         destArr = result
         if len(destArr) < len(source):
-            return [] 
+            return []
         print(result)
         # kopire alt andet end sidste element til srcArr og beholder robot start position
         source.extend(destArr[:-1])
@@ -130,20 +133,18 @@ class CVGrid:
                 res = a_star_search(grid, source[i], special_dest)
                 path.extend()
                 if i + 1 < len(destArr):
-                    print(f"Running A* search from {special_dest} to {destArr[i]}")
+                    print(
+                        f"Running A* search from {special_dest} to {destArr[i]}")
                     res = a_star_search(grid, special_dest, destArr[i])
                 i += 1  # Move to the next source and destination
             else:
                 dest = destArr[i]
                 print(f"Running A* search from {src} to {dest}")
                 res = a_star_search(grid, src, dest)
-                i += 1 
+                i += 1
             if res is not None:
                 path.extend(res)
         return path
-
-
-
 
 
 class CVCell:
