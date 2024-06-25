af9e68759282c6248cbcdf7a2f7d29680ef557ae
Mads Nielsen <55346271+madsvnielsen@users.noreply.github.com>
2024-06-25 13:24:19 +0200

    Cleanup

---
+++ client.py
@@ -0,0 +1,100 @@
+import socket
+
+from ev3dev2.auto import *
+from ev3dev2.motor import MoveTank
+import time
+
+m = Motor(OUTPUT_C)
+motors= MoveTank(OUTPUT_A,OUTPUT_B)
+
+#gyro =GyroSensor(INPUT_1)
+#motors.gyro =gyro
+#motors.gyro.calibrate()
+#target_host = "192.168.4.170" # Change this to the IP address of your server
+#target_host = "172.20.10.13"
+target_host = "192.168.13.48"
+#target_host = "192.168.75.248"
+target_port = 27700 # Change this to the port of your server
+
+# create a socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.settimeout(5)
+# connect to the server
+client.connect((target_host, target_port))
+
+
+def start() -> None:
+    m.on(100)
+
+def moveBackward() -> None:
+    motors.on_for_seconds(20, 20, 0.75)
+
+def moveFoward(secs) -> None:
+    motors.on_for_seconds(-20, -20, secs)
+
+def turnRight() -> None:
+    motors.on(-10,10)
+    #motors.on_for_seconds(-10, 10, secs)
+
+def turnLeft() -> None:
+    motors.on(10,-10)
+    #motors.on_for_seconds(10, -10, secs)
+
+def turnRightMicro(secs) -> None:
+    motors.on_for_seconds(-10, 10, secs)
+
+def turnLeftMicro(secs) -> None:
+    motors.on_for_seconds(10, -10, secs)
+
+
+def dumpBalls() -> None:
+    m.stop()
+    m.on_for_seconds(-100, 10)
+    m.on(100)
+
+def stop() -> None:
+    m.stop()
+    motors.stop()
+
+
+def react(command):
+    global speed, turn
+    args = command.split(" ")
+    c = args[0]
+    try:
+        if c in [ord('q'), 27, ord('p')]:
+            return
+        elif c == "left": #leftkey
+            turnLeft()
+        elif c == "right": #rightkey
+            turnRight()
+        elif c == "mleft":
+            turnLeftMicro(float(args[1]))
+        elif c == "mright":
+            turnRightMicro(float(args[1]))
+        elif c == "forward": # up key
+            moveFoward(float(args[1]))
+        elif c == "back": #down key
+            moveBackward()
+        elif c == "k":
+            dumpBalls()
+        elif c == "stop":
+            stop()
+        elif c == "start":
+            start()
+    except:
+        print("Error")
+# receive
+while True:
+    try:
+        # receive data from the server
+        response = client.recv(4096)
+        if not response:  # If the response is empty, the server closed the connection
+            print("Server closed the connection.")
+            break
+        # decode and print the received data
+        print(response.decode())
+        react(response.decode())
+    except socket.timeout:  # Handle timeout
+        pass
+# send
